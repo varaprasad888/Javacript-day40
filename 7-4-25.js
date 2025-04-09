f2ec3fec08de8e196248d15c34bc74d8faaf4285
varaprasad888 <podilavaraprasad39@gmail.com>
2025-04-09 13:03:16 +0530

    Add files via upload

---
+++ 7-4-25.js
@@ -0,0 +1,33 @@
+//string methods-
+
+let str="Ravi sashthri"
+console.log(str)
+
+let upper="Hello javascript"
+console.log(upper.toUpperCase())
+
+let lower = "SAMPLE CODE"
+console.log(lower.toLowerCase())
+
+let text = "hello";
+console.log(text.charAt(1))
+
+let trim = "   hello   ";
+console.log(trim.trim());
+
+let text1 = "hello world";
+console.log(text1.startsWith("hello")); 
+console.log(text1.startsWith("world"));
+
+let text2 = "hello world";
+console.log(text2.endsWith("world"));
+console.log(text2.endsWith("hello"));
+
+let text3 = "hello world";
+console.log(text3.indexOf("world")); 
+console.log(text3.indexOf("JavaScript"));
+
+let text4 = "hello world";
+console.log(text4.includes("world"));  
+console.log(text4.includes("JavaScript"));
+
